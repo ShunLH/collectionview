2e49b0f1b02e967942339a6fc61745cb52d75d6c
ShunLH <shunleihmu.shunlh@gmail.com>
2019-08-31 15:50:15 +0630

    Initial Commit

---
+++ CollectionViewExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewExercise
+//
+//  Created by AcePlus Admin on 8/31/19.
+//  Copyright © 2019 SLH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
